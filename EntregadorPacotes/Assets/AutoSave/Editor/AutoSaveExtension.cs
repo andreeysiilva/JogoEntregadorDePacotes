844f4aea5a2d91ac0688ebd8fd633b210a289dcf
Andrey Silva <82220976+andreeysiilva@users.noreply.github.com>
2022-01-16 01:15:25 -0300

    Adicionado sistema de entrega e coleta

--- EntregadorPacotes/Assets/AutoSave/Editor/AutoSaveExtension.cs
+++ EntregadorPacotes/Assets/AutoSave/Editor/AutoSaveExtension.cs
@@ -1,26 +1,16 @@
 ﻿using UnityEditor;
-using UnityEngine;
+using UnityEditor.SceneManagement;
 
-namespace EckTechGames
-{
-	[InitializeOnLoad]
-	public class AutoSaveExtension
-	{
-		// Static constructor that gets called when unity fires up.
-		static AutoSaveExtension()
-		{
-			EditorApplication.playmodeStateChanged += AutoSaveWhenPlaymodeStarts;
-		}
-
-		private static void AutoSaveWhenPlaymodeStarts()
-		{
-			// If we're about to run the scene...
-			if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
-			{
-				// Save the scene and the assets.
-				EditorApplication.SaveScene();
-				AssetDatabase.SaveAssets();
-			}
-		}
-	}
+[InitializeOnLoad]
+public class AutoSaveExtension {
+    static AutoSaveExtension() {
+        EditorApplication.playModeStateChanged += AutoSaveWhenPlaymodeStarts;
+    }
+	
+    private static void AutoSaveWhenPlaymodeStarts(PlayModeStateChange state) {
+        if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying) {
+            EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene(), null, false);
+            AssetDatabase.SaveAssets();
+        }
+    }
 }
---
+++ EntregadorPacotes/Assets/Delivery.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+
+    [SerializeField] Color32 corDoPacote = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 semCorPacote = new Color32 (1, 1, 1, 1);
+    [SerializeField] float tempoDestruicao = 0.5f;
+    bool temPacote;
+
+    SpriteRenderer redenrizadorSprite;
+
+    void Start() {
+        redenrizadorSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Aqui tem colisão");
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Pacote" && !temPacote)
+        {
+            Debug.Log("Pacote Recolhido");
+            temPacote = true;
+            redenrizadorSprite.color = corDoPacote;
+            Destroy(other.gameObject, tempoDestruicao);
+        }
+
+        if (other.tag == "Entrega" && temPacote)
+        {
+            Debug.Log("Pacote Entregue");
+            temPacote = false;
+            redenrizadorSprite.color = semCorPacote;
+            Destroy(other.gameObject, tempoDestruicao);
+        }
+    }
+}
